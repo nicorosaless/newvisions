b52b5a9131f1328bd7fb721a63c153f5b22d265a
LatorreSony <alex.latorre@sony.com>
2025-09-12 18:30:07 +0200

    First frontend push

---
+++ frontend/vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: {
+        name: 'Visions',
+        short_name: 'Visions',
+        description: 'A Progressive Web App built with Vite',
+        theme_color: '#242424',
+        background_color: '#242424',
+        display: 'fullscreen',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+})
